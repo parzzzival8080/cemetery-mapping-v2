e0c62c10ab404ee5bc3c613f4a929c2ffcb8ba52
parzzzival8080 <54020721+parzzzival8080@users.noreply.github.com>
2021-12-19 13:10:08 +0800

    fixes

--- database/migrations/2021_07_14_170423_create_occupants_table.php
+++ database/migrations/2021_07_14_170423_create_occupants_table.php
@@ -19,6 +19,7 @@ class CreateOccupantsTable extends Migration
             $table->string('status');
             $table->string('number');
             $table->string('address');
+            $table->string('type');
             $table->unsignedBigInteger('user_id');
             $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
             $table->timestamps();
